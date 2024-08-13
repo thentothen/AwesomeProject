e1577f2eb42c2ff5dc45abfa5235eab1c26b22d3
cjq <chenjiaqing10@163.com>
2024-08-13 11:15:58 +0800

    不需要add Android包

--- android/app/src/main/java/com/awesomeproject/MainApplication.kt
+++ android/app/src/main/java/com/awesomeproject/MainApplication.kt
@@ -26,7 +26,7 @@ class MainApplication : Application(), ReactApplication {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
               // add(SplashScreenReactPackage())
-              add(VectorIconsPackage())
+              // add(VectorIconsPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
